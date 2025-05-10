01a67215bf7abff824bc344bba9580048e61b611
Phan Van Phat <phanphatdz@gmail.com>
2025-05-10 23:23:32 +0700

    Order update

--- Project/src/Order.java
+++ Project/src/Order.java
@@ -1,19 +1,17 @@
-import java.util.List;
-import java.util.ArrayList;
 import java.time.LocalDate;
 
 public class Order {
     private String orderId;
     private String userId;
-    private List<Product> products;
+    private String productId;
     private double totalAmount;
     private LocalDate orderDate;
     private String status;
 
-    public Order(String orderId, String userId) {
+    public Order(String orderId, String userId, String productId) {
         this.orderId = orderId;
         this.userId = userId;
-        this.products = new ArrayList<>();
+        this.productId = productId;
         this.orderDate = LocalDate.now();
         this.status = "Chờ xác nhận";
     }
