ce261aa6c26ce429aa7998edafc8f8164781b169
Phan Van Phat <phanphatdz@gmail.com>
2025-05-10 22:52:38 +0700

    update order

--- Project/src/Order.java
+++ Project/src/Order.java
@@ -17,33 +17,4 @@ public class Order {
         this.orderDate = LocalDate.now();
         this.status = "Chờ xác nhận";
     }
-
-    
-    public void addProduct(Product product) {
-        products.add(product);
-        totalAmount += product.getTotalPrice();
-    }
-
-    
-    public void displayOrder() {
-        System.out.println("Mã đơn hàng: " + orderId);
-        System.out.println("Khách hàng: " + userId);
-        System.out.println("Ngày đặt hàng: " + orderDate);
-        System.out.println("Trạng thái đơn hàng: " + status);
-        System.out.println("Danh sách sản phẩm:");
-        for (Product product : products) {
-            System.out.println(" - " + product);
-        }
-        System.out.println("Tổng tiền: " + totalAmount + " VNĐ");
-    }
-
-    // Cập nhật trạng thái đơn hàng
-    public void updateStatus(String newStatus) {
-        this.status = newStatus;
-    }
-
-    // Tính tổng số tiền
-    public double getTotalAmount() {
-        return totalAmount;
-    }
 }
