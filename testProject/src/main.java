6e537ed92e4483dd623183feb170264a19828f11
Phan Van Phat <phanphatdz@gmail.com>
2025-05-10 13:00:51 +0700

    update

---
+++ testProject/src/main.java
@@ -0,0 +1,9 @@
+
+public class main {
+    public static void main(String[] args) {
+        user u = new user();
+        u.name = "John Doe";
+        u.say();
+    }
+
+}
---
+++ testProject/src/user.java
@@ -0,0 +1,13 @@
+
+public class user {
+    public String name;
+    public String email;
+    public String password;
+    public String phone;
+    public String address;
+    public int age;
+    public void say(){
+        System.out.println("Mua di");
+    }
+
+}
