9ef2481ec6deaeae9978f04c124dce6e441bc9f9
Phan Van Phat <phanphatdz@gmail.com>
2025-05-11 15:39:39 +0700

    ok

--- Project/test/TestOrder.java
+++ Project/test/TestOrder.java
@@ -2,9 +2,6 @@ import java.util.Arrays;
 
 public class TestOrder {
     public static void main(String[] args) {
-        // Create product and order sample
-        Product product = new Product(101, 10, "Smartphone", 5000000, 50, "High-end smartphone");
-        product.displayProductInfo();
 
         // Create an order with a list of product IDs
         Order order = new Order("ORD001", "USER123", Arrays.asList(101, 102));
